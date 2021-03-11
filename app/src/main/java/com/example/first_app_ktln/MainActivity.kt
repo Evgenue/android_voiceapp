239e1da25188fc5538f752d9fe6117e487d3b533
Кучерявый Евгений <ku>
2021-03-11 20:28:07 +0800

    android app

---
+++ app/src/main/java/com/example/first_app_ktln/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.first_app_ktln
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("netology voice", "start of onCreate function")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.topAppBar))
+
+
+        Log.d("netology voice", "end of onCreate function")
+
+    }
+}
